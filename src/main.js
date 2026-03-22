2593a537874217d5eb363e78b2042770848bf046
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2026-03-23 00:00:34 +0100

    fixed absolute URL issue in base url

--- src/main.js
+++ src/main.js
@@ -5,7 +5,4 @@ import { ViteSSG } from "vite-ssg";
 import App from "./App.vue";
 import { routes } from "./router/router";
 
-export const createApp = ViteSSG(App, {
-  routes,
-  base: import.meta.env.BASE_URL,
-});
+export const createApp = ViteSSG(App, { routes });
