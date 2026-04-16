a1ec9d29b018c8a8b78864b67252e244356825d1
Amon Benson <amon.k.benson@campus.tu-berlin.de>
2026-04-16 20:35:51 +0200

    Added homepage alias

--- src/router/router.js
+++ src/router/router.js
@@ -2,6 +2,6 @@ import ComeFromAwayView from "@/views/cfa/Index.vue";
 import HomeView from "@/views/home/Index.vue";
 
 export const routes = [
-  { name: "home", path: "/", component: HomeView },
+  { name: "home", path: "/", alias: "/index.html", component: HomeView },
   { name: "cfa", path: "/cfa", component: ComeFromAwayView },
 ];
